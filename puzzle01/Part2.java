97b6967e983c85d8f424005a9fd2104677fc954c
Stga <stawgzilla@gmail.com>
2017-12-02 10:19:56 +0000

    Cleanup and optimization.

--- puzzle01/Part2.java
+++ puzzle01/Part2.java
@@ -1,4 +1,9 @@
-public class Testing {
+//*
+//Part2 checks input x against its couterpart halfway through the input
+//string. The input string is circular so the second half need to be tested
+//against the first half due to wrap-around.
+//*
+public class A {
   public static void main(String[] args) {
     long startTime = System.nanoTime();
 
@@ -8,15 +13,15 @@ public class Testing {
     int lenHalved = inputLength/2;
     int[] inputValues = new int[inputLength];
 
-    for(int i=0;i<inputLength; i++) {
-      inputValues[i] = input.charAt(i) - 48;
+    for(int i=0;i<inputLength; i++) {	  
+      inputValues[i] = input.charAt(i) - 48; //adjust for ascii
     }
 
-    for(int i=0; i<inputLength; i++) {
-        int halfway = (i > lenHalved-1) ? (i-lenHalved) : (i+lenHalved);
-
-	if(inputValues[i] == inputValues[halfway]) {
-	  sum += inputValues[i];
+    //Testing a==b and b==a is redundant. Iterate through the first
+    //half of the list and double any match.
+    for(int i=0; i<lenHalved; i++) {
+	if(inputValues[i] == inputValues[i+lenHalved]) {
+	  sum += inputValues[i]*2;
 	}
     }
     
