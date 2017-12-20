a3818c64f14315d17471aa8fd5304ce8fe6d5fb7
Stga <stawgzilla@gmail.com>
2017-12-20 18:10:38 +0000

    P2

--- puzzle18/DuetAnalyzer.java
+++ puzzle18/DuetAnalyzer.java
@@ -1,23 +1,37 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class DuetAnalyzer {
-	private Map<String, Integer> registerValues;
-	private List<Integer> previousFrequencies;
+public class DuetAnalyzer extends Thread {
+    private Map<String, Long> registerValues;
+    private List<Long> previousFrequencies;
 	private List<String> instructions;
-	
-	DuetAnalyzer() {
+    private ArrayDeque<Long> queue;
+    private int programId;
+    private DuetAnalyzer otherProgram;
+
+    DuetAnalyzer(int pid) {
 		registerValues = new HashMap<>();
 		previousFrequencies = new ArrayList<>();
 		instructions = new ArrayList<>();
-	}
-	
-	private void setRegisterValue(String registerName, int newValue) {
+        queue = new ArrayDeque<>();
+        programId = pid;
+    }
+
+    public void setOtherProgram(DuetAnalyzer otherProgram) {
+        this.otherProgram = otherProgram;
+    }
+
+    public void putInQueue(long l) {
+        this.otherProgram.queue.addLast(l);
+    }
+
+    public int getProgramId() {
+        return this.programId;
+    }
+
+    public void setRegisterValue(String registerName, long newValue) {
 		this.registerValues.put(registerName, newValue);
 	}
 	
@@ -39,13 +53,11 @@ public class DuetAnalyzer {
 		}
 	}
 
-	public int getFirstRecoveredFrequency() {
+    public long getFirstRecoveredFrequency() {
 		int instructionPointer = 0;
 		
 		while(true) {
 			String[] instructionComponents = this.instructions.get(instructionPointer).split(" ");
-
-			System.out.println(this.instructions.get(instructionPointer));
 
 			if(instructionComponents.length > 2) {
 				boolean isNumber = instructionComponents[2].matches("[-+]?\\d*\\.?\\d+");
@@ -61,25 +73,25 @@ public class DuetAnalyzer {
 					break;
 					
 				case "set":
-					int newValue = Integer.parseInt(instructionComponents[2]);
+                    long newValue = Integer.parseInt(instructionComponents[2]);
 					this.setRegisterValue(instructionComponents[1], newValue);
 					instructionPointer++;
 					break;
 					
 				case "add":
-					int addValue = Integer.parseInt(instructionComponents[2]);
+                    long addValue = Integer.parseInt(instructionComponents[2]);
 					this.add(instructionComponents[1], addValue);
 					instructionPointer++;
 					break;
 					
 				case "mul":
-					int mulValue = Integer.parseInt(instructionComponents[2]);
+                    long mulValue = Integer.parseInt(instructionComponents[2]);
 					this.mul(instructionComponents[1], mulValue);
 					instructionPointer++;
 					break;
 					
 				case "mod":
-					int modValue = Integer.parseInt(instructionComponents[2]);
+                    long modValue = Integer.parseInt(instructionComponents[2]);
 					this.mod(instructionComponents[1], modValue);
 					instructionPointer++;
 					break;
@@ -101,28 +113,100 @@ public class DuetAnalyzer {
 			}
 		}
 	}
-	
+
+    public void getProgramSendCount() {
+        int sendCount = 0;
+
+        int instructionPointer = 0;
+
+        while (true) {
+            String[] instructionComponents = this.instructions.get(instructionPointer).split(" ");
+
+            if (instructionComponents.length > 2) {
+                boolean isNumber = instructionComponents[2].matches("[-+]?\\d*\\.?\\d+");
+                if (!isNumber) {
+                    instructionComponents[2] = "" + this.registerValues.get(instructionComponents[2]);
+                }
+            }
+
+            switch (instructionComponents[0]) {
+                case "snd":
+                    long regValue = this.registerValues.get(instructionComponents[1]);
+                    this.putInQueue(regValue);
+                    instructionPointer++;
+                    sendCount++;
+                    break;
+
+                case "set":
+                    long newValue = Integer.parseInt(instructionComponents[2]);
+                    this.setRegisterValue(instructionComponents[1], newValue);
+                    instructionPointer++;
+                    break;
+
+                case "add":
+                    long addValue = Integer.parseInt(instructionComponents[2]);
+                    this.add(instructionComponents[1], addValue);
+                    instructionPointer++;
+                    break;
+
+                case "mul":
+                    long mulValue = Integer.parseInt(instructionComponents[2]);
+                    this.mul(instructionComponents[1], mulValue);
+                    instructionPointer++;
+                    break;
+
+                case "mod":
+                    long modValue = Integer.parseInt(instructionComponents[2]);
+                    this.mod(instructionComponents[1], modValue);
+                    instructionPointer++;
+                    break;
+
+                case "rcv":
+                    while (queue.size() == 0) {
+                        if (this.programId == 1) {
+                            System.out.println(String.format("Program %d halted, SendCount %d", this.programId, sendCount));
+                        }
+                    }
+                    registerValues.put(instructionComponents[1], queue.pop());
+                    instructionPointer++;
+                    break;
+
+                case "jgz":
+                    if (this.shouldJgz(instructionComponents[1])) {
+                        instructionPointer += Integer.parseInt(instructionComponents[2]);
+                    } else {
+                        instructionPointer++;
+                    }
+                    break;
+            }
+        }
+    }
+
 	private void playSound(String register) {
-		int soundFrequency = this.registerValues.get(register);
+        long soundFrequency = this.registerValues.get(register);
 		this.previousFrequencies.add(soundFrequency);
 	}
-	
-	private void add(String register, int addValue) {
-		int currentRegisterValue = this.registerValues.get(register);
+
+    private void add(String register, long addValue) {
+        long currentRegisterValue = this.registerValues.get(register);
 		this.registerValues.put(register, currentRegisterValue+addValue);
 	}
-	
-	private void mul(String register, int mulValue) {
-		int currentRegisterValue = this.registerValues.get(register);
+
+    private void mul(String register, long mulValue) {
+        long currentRegisterValue = this.registerValues.get(register);
 		this.registerValues.put(register, currentRegisterValue*mulValue);
 	}
-	
-	private void mod(String register, int modValue) {
-		int currentRegisterValue = this.registerValues.get(register);
+
+    private void mod(String register, long modValue) {
+        long currentRegisterValue = this.registerValues.get(register);
 		this.registerValues.put(register, currentRegisterValue%modValue);
 	}
 	
 	private boolean shouldJgz(String register) {
-		return this.registerValues.get(register) > 0;
-	}
+        return this.registerValues.get(register) > 0;
+    }
+
+    public void run() {
+        this.getProgramSendCount();
+    }
 }
--- puzzle18/Puzzle18Main.java
+++ puzzle18/Puzzle18Main.java
@@ -1,7 +1,22 @@
 public class Puzzle18Main {
 	public static void main(String[] args) {
-		DuetAnalyzer da = new DuetAnalyzer();
+        DuetAnalyzer da = new DuetAnalyzer(0);
         da.copyInstructions("./puzzle18/puzzle18Input.txt");
-		System.out.println(da.getFirstRecoveredFrequency()); 
+        System.out.println(da.getFirstRecoveredFrequency());
+
+        DuetAnalyzer da1 = new DuetAnalyzer(0);
+        DuetAnalyzer da2 = new DuetAnalyzer(1);
+
+        da1.copyInstructions("./puzzle18/puzzle18Input.txt");
+        da2.copyInstructions("./puzzle18/puzzle18Input.txt");
+
+        da1.setOtherProgram(da2);
+        da2.setOtherProgram(da1);
+
+        da1.setRegisterValue("p", da1.getProgramId());
+        da2.setRegisterValue("p", da1.getProgramId());
+
+        da1.start();
+        da2.start();
 	}
 }
